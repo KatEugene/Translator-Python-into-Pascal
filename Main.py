ef0789fbabcadb18cfd0d5f0ad3ba8e902cd922b
Eugene <jea.kaanik@gmail.com>
2020-02-02 22:18:27 +0300

    Beginning of project started. Added main structure of project and of main code

---
+++ Main.py
@@ -0,0 +1,13 @@
+# Импортируем все необходимые библиотеки
+import sys
+
+# Считываем данные
+py_code = sys.stdin.read().split("\n")
+pascal_code = []
+
+# Обрабатываем код из питона в паскаль
+for string in py_code:
+    pass
+
+# Выводим код на паскале
+
